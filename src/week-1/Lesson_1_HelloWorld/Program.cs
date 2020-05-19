2dcef28635fd8290faf81123f3992955ce22e8d2
Dara Oladapo <daraoladapo@outlook.com>
2020-05-19 18:39:25 +0100

    start01

---
+++ src/week-1/Lesson_1_HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿//importing System namespace (family name)
+using System;
+
+//family name of our project
+namespace Lesson_1_HelloWorld
+{
+    //entry point of our project
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Code Nest Africa");
+        }
+
+        //explaining main method with rice
+        // static void MakeRice(){
+
+        // }
+        // static void MakeRice(int OfadaRice){
+
+        // }
+        // static void MakeRice(double Basmati){
+            
+        // }
+    }
+}
