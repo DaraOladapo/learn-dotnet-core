e8bb06205dd4210f4d58a389c36c014a3328dca6
Dara Oladapo <daraoladapo@outlook.com>
2020-05-21 18:12:19 +0100

    up until CTS

---
+++ src/week-1/Lesson_10_Interfaces/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_10_Interfaces
+{
+    class Program:IMyInterface
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void MyInterfaceMethod(){
+
+        }
+    }
+    public interface IMyInterface
+    {
+        public void MyInterfaceMethod();
+    }
+}
