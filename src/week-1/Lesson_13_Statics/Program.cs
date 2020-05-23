5909458542981de0cd7fdcc700cf65ad8574fccd
Dara Oladapo <daraoladapo@outlook.com>
2020-05-23 19:11:13 +0100

    statics

---
+++ src/week-1/Lesson_13_Statics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson_13_Statics {
+    class Program {
+        static void Main(string[] args) {
+            Vehicle.Run();
+            Vehicle.Model="Micra";
+
+            var MyVehicle=new Vehicle();
+            MyVehicle.Name="Toyota";
+            MyVehicle.Stop();
+        }
+    }
+
+    public class Vehicle {
+        public string Name {
+            get;
+            set;
+        }
+        public static string Model{get;set;}
+        public static void Run() {
+            Console.WriteLine("I'm running static");
+        }
+
+        public void Stop() {
+            Console.WriteLine("I'm running initialized");
+        }
+
+    }
+}
