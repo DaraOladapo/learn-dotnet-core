1f9c7ec5a18e5eff0279566801043e64149679fa
Dara Oladapo <daraoladapo@outlook.com>
2020-06-02 18:12:06 +0100

    Day 7

---
+++ src/week-3/Lesson_22_TryParsing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson_22_TryParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MyNumber;
+            Console.WriteLine("Enter a number");
+            string UserInput=Console.ReadLine();
+            var GetInput=int.TryParse(UserInput, out MyNumber);
+            if(GetInput==true)
+            {
+                Console.WriteLine($"Successfully parsed {UserInput} from {UserInput.GetType()} to {MyNumber} of type {MyNumber.GetType()}");
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed");
+            }
+        }
+    }
+}
---
+++ src/week-3/Lesson_23_TryAndCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson_23_TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int MyNumber;
+         Console.WriteLine("Enter a number");
+         var UserInput=Console.ReadLine();
+         try
+         {
+             MyNumber=int.Parse(UserInput);
+             Console.WriteLine(MyNumber);
+         }
+         catch (System.Exception Exp)
+         {
+            Console.WriteLine("We don't understand your input. Try again");
+            // Console.WriteLine("An exception was thrown. Here are the details.");
+            // Console.WriteLine($"Message: {Exp.Message}");
+            // Console.WriteLine($"Help Link: {Exp.HelpLink}");
+            // Console.WriteLine($"Stack Trace: {Exp.StackTrace.ToString()}");
+         }
+         finally
+         {
+             Console.WriteLine("Thank you for banking with us.");
+         }
+        }
+    }
+}
