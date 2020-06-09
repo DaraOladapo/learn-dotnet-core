cc0902406e3251c3a098ebc58dfef4e738aa9424
Dara Oladapo <daraoladapo@outlook.com>
2020-06-09 19:10:33 +0100

    Week 4-01

---
+++ src/week-4/Lesson_29_ASPNETMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson_29_ASPNETMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
