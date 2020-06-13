430ccbc5f5e596b84090280b014f8dae78f8ea24
Dara Oladapo <daraoladapo@outlook.com>
2020-06-13 19:11:03 +0100

    Day 12 Done

---
+++ src/week-5-8/Portal/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portal.Models;
+using Portal.Models.Data;
+
+namespace Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var Student = new Student() { FirstName = "Dara", LastName = "Oladapo" };
+            return View(Student);
+        }
+
+        [NonAction]
+        public void SayHi()
+        {
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
