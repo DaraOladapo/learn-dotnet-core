9aace4b7819fb47d42f6ab967c9eb5830e5c73c0
Dara Oladapo <daraoladapo@outlook.com>
2020-06-16 19:14:43 +0100

    Heleprs

--- src/week-5-8/Portal/Controllers/StudentController.cs
+++ src/week-5-8/Portal/Controllers/StudentController.cs
@@ -2,15 +2,94 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Portal.Models.Data;
 
 namespace Portal.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        List<Student> _Students = new List<Student>() {
+            new Student(){ FirstName = "Dara", LastName = "Oladapo", EmailAddress = "me@daraoladapo.com", ID = 1 },
+            new Student(){ FirstName = "Oluwadara", LastName = "Oladapo", EmailAddress = "dara@daraoladapo.com", ID = 2 },
+        };
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            return View(_Students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(long id)
+        {
+            var _Student = _Students.FirstOrDefault(opt => opt.ID == id);
+            return View(_Student);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var _Student = _Students.FirstOrDefault(opt => opt.ID == id);
+            return View(_Student);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                var _Student = _Students.FirstOrDefault(opt => opt.ID == id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- src/week-5-8/Portal/Models/Data/Student.cs
+++ src/week-5-8/Portal/Models/Data/Student.cs
@@ -7,8 +7,10 @@ namespace Portal.Models.Data
 {
     public class Student
     {
+        public long ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName => $"{FirstName} {LastName}";
+        public string EmailAddress { get; set; }
     }
 }
